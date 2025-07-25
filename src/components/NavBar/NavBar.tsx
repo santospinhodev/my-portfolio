ce38d034d552fc389a4dc30fb0ea80b2b5ba548b
contato.rodrigosantosdev@gmail.com <contato.rodrigosantosdev@gmail.com>
2025-07-25 01:33:06 -0300

    feat: init NavBar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,22 @@
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+
+const NavBar = () => {
+    const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+        display: 'flex',
+        justifyContent: 'space-evenly',
+    }))
+
+    return (
+        <>
+            <AppBar position="absolute">
+                <StyledToolbar>
+                    <MenuItem>About</MenuItem>
+                    <MenuItem>Skills</MenuItem>
+                    <MenuItem>Projects</MenuItem>
+                </StyledToolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar
